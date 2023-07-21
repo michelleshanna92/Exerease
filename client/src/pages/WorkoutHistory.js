64b0efb4a47799d2cb0de257a276ce27349ef7c7
Japheth <japhethbruce93@gmail.com>
2023-07-21 09:39:27 -0500

    added open string for seeding

--- client/src/pages/WorkoutHistory.js
+++ client/src/pages/WorkoutHistory.js
@@ -1,3 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const data = {
+  id: "",
+  name: "",
+  description: "",
+  muscleType: "",
+  duration: "",
+  exercises: [
+  { id: 1, name: "", sets: "", reps: "" },
+  { id: 2, name: "", sets: "", reps: "" },
+  { id: 3, name: "", sets: "", reps: "" },
+  { id: 4, name: "", sets: "", reps: "" },
+  { id: 5, name: "", sets: "", reps: "" },
+  { id: 6, name: "", sets: "", reps: "" },
+  { id: 7, name: "", sets: "", reps: "" },
+  ],
+  };
+  
