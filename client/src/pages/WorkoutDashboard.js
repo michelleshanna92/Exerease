c6dc9370fd226b4e8cdc2af2eb94345f8053a177
unknown <edtread3@gmail.com>
2023-07-24 19:21:11 -0400

    working on adding a Calendar component for workout Dashboard

--- client/src/pages/WorkoutDashboard.js
+++ client/src/pages/WorkoutDashboard.js
@@ -5,7 +5,9 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
 import { GET_WORKOUT_HISTORY } from '../utils/queries'; 
+import { GET_WORKOUT_GOAL } from '../utils/queries';
 
+// Workout History data with Calendar
 function WorkoutDashboard() {
   const [selectedDate, setSelectedDate] = useState(null);
 
@@ -20,12 +22,23 @@ function WorkoutDashboard() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const card = {
+    width: '20rem',
+    color: 'white',
+    backgroundColor: '#292929',
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+  
+  }
+ // Return Calendar with workouthistory data and dates
   return (
-    <div>
-      <h1>Workout Dashboard</h1>
+    <div className="card text-center">
+    <div className="card">
+      <h2>"Your WorkOut History!"</h2>
 
       <div>
-        <h2>Calendar</h2>
+        <h3>Calendar</h3>
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
@@ -56,7 +69,63 @@ function WorkoutDashboard() {
         </div>
       )}
     </div>
+  </div>
+  );
+
+ 
+  
+};
+
+ // return Profile card
+const Profile = ({ user }) => {
+
+  const { loading, error, data } = useQuery(GET_WORKOUT_GOAL, {
+   // variables: { selectedDate: selectedDate?.toISOString() },
+  });
+
+  const cardTwo = {
+    width: '20rem',
+    color: 'white',
+    backgroundColor: '#292929'
+  };
+
+
+
+  return (
+    <><div className="card-hearder bg-primary text-white">
+      <h1>Welcome, {user.name}!</h1>
+    </div>
+    <div className="cardTwo-body">
+
+        <h2>Your Profile:</h2>
+        <p>
+          <strong>Name:</strong> {user.name}
+        </p>
+        <p>
+          <strong>Height:</strong> {user.height} cm
+        </p>
+        <p>
+          <strong>Weight:</strong> {user.weight} kg
+        </p>
+        <p>
+          <strong>Workout Goal:</strong> {user.workoutGoal}
+        </p>
+      </div>
+      <div>
+        <h3>Your Workout History:</h3>
+        {<Calendar value={user.workoutHistory.map((workout) => new Date(workout.date))} />}
+
+      </div></>
+  
+  
   );
 }
+
+
+
+
+
+
+
 
 export default WorkoutDashboard;
